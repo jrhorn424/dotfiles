f6a003092628bdb1c8e5fb2107857742d7d447ee
Jeffrey Horn <jrhorn424@gmail.com>
2013-09-23 16:38:53 -0400

    Add clip function from Terpstra

--- home/.functions.zsh
+++ home/.functions.zsh
@@ -1,3 +1,13 @@
+function clip() {
+  type=`file "$1"|grep -c text`
+  if [ $type -gt 0 ]; then
+    cat "$@"|pbcopy
+    echo "Contents of $1 are in the clipboard."
+  else
+    echo "File \"$1\" is not plain text."
+  fi
+}
+
 function any() { # grep through running processes
     emulate -L zsh
     unsetopt KSH_ARRAYS
