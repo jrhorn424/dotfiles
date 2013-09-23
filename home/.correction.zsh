80769af90059a7d17c71e677231756a1ea9ffae3
Jeffrey Horn <jrhorn424@gmail.com>
2013-09-23 03:03:48 -0400

    Disable zsh correction

--- home/.correction.zsh
+++ home/.correction.zsh
@@ -1,10 +1,10 @@
-setopt correct_all
+# disable correction
+DISABLE_CORRECTION="true"
+unsetopt correct
+unsetopt correctall
+unsetopt correct_all # for older zsh's
+setopt nocorrectall
 
-alias sudo="nocorrect sudo"
-alias man='nocorrect man'
-alias mv='nocorrect mv'
-alias mkdir='nocorrect mkdir'
-
-[[ -x "$(which knife)" ]]    && alias knife="nocorrect knife"
-[[ -x "$(which aptitude)" ]] && alias aptitude="nocorrect aptitude"
-[[ -x "$(which mysql)" ]]    && alias mysql='nocorrect mysql'
+# enable correction
+# setopt correct # spelling correction for commands
+# setopt correctall # spelling correction for arguments
