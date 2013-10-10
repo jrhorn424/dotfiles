1589a94f4076cfc9fcd15b02401801d227872af7
Jeffrey Horn <jrhorn424@gmail.com>
2013-10-10 15:44:37 -0400

    Add quote function

--- home/.functions.zsh
+++ home/.functions.zsh
@@ -34,3 +34,11 @@ function whatip() {
 function cdc() {
     cd $1; ls
 }
+
+quote () {
+  Q=$(curl -s --connect-timeout 2 "http://www.quotationspage.com/random.php3" | grep -m 1 "dt ")
+  TXT=$(echo "$Q" | sed -e 's/<\/dt>.*//g' -e 's/.*html//g' -e 's/^[^a-zA-Z]*//' -e 's/<\/a..*$//g')
+  W=$(echo "$Q" | sed -e 's/.*\/quotes\///g' -e 's/<.*//g' -e 's/.*">//g'"')
+    echo "\e[0;33m${W}\e[0;30m: \e[0;35m“${TXT}”\e[m"
+  }"]]]]"
+}
