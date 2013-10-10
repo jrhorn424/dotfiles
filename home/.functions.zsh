1be78080415c0c29a5b280b3e8ed90d3a745f625
Jeffrey Horn <jrhorn424@gmail.com>
2013-10-10 15:47:23 -0400

    Fix typos

--- home/.functions.zsh
+++ home/.functions.zsh
@@ -38,7 +38,6 @@ function cdc() {
 quote () {
   Q=$(curl -s --connect-timeout 2 "http://www.quotationspage.com/random.php3" | grep -m 1 "dt ")
   TXT=$(echo "$Q" | sed -e 's/<\/dt>.*//g' -e 's/.*html//g' -e 's/^[^a-zA-Z]*//' -e 's/<\/a..*$//g')
-  W=$(echo "$Q" | sed -e 's/.*\/quotes\///g' -e 's/<.*//g' -e 's/.*">//g'"')
-    echo "\e[0;33m${W}\e[0;30m: \e[0;35m“${TXT}”\e[m"
-  }"]]]]"
+  W=$(echo "$Q" | sed -e 's/.*\/quotes\///g' -e 's/<.*//g' -e 's/.*">//g')
+  echo "\e[0;33m${W}\e[0;30m: \e[0;35m“${TXT}”\e[m"
 }
