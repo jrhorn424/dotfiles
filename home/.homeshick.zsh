a672eba7f4b2649ecc1b9b97768dc58aafe1a277
Jeffrey Horn <jrhorn424@gmail.com>
2013-11-10 19:24:54 -0500

    Alias commands

--- home/.homeshick.zsh
+++ home/.homeshick.zsh
@@ -1,1 +1,8 @@
 source $HOME/.homesick/repos/homeshick/homeshick.sh
+alias hs='homeshick'
+alias hst='homeshick track'
+alias hsp='homeshick pull'
+alias hsc='homeshick check'
+alias hscd='homeshick cd'
+alias hsl='homeshick link'
+alias hsls='homshick list'
