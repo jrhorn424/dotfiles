4766b4b4edadd1fcd5b5bc7287be7688a5775534
Jeffrey Horn <jeff@jrhorn.me>
2018-09-23 19:04:10 -0700

    Customize history options, uppercase vars

--- home/.history.zsh
+++ home/.history.zsh
@@ -1,13 +1,17 @@
-setopt append_history # Allow multiple terminal sessions to all append to one zsh command history
-setopt extended_history # save timestamp of command and duration
-setopt inc_append_history # Add comamnds as they are typed, don't wait until shell exit
-setopt hist_expire_dups_first # when trimming history, lose oldest duplicates first
-setopt hist_ignore_dups # Do not write events to history that are duplicates of previous events
-setopt hist_ignore_space # remove command line from history list when first character on the line is a space
-setopt hist_find_no_dups # When searching history don't display results already cycled through twice
-setopt hist_reduce_blanks # Remove extra blanks from each command line being added to history
-setopt hist_verify # don't execute, just expand history
-setopt share_history # imports new commands and appends typed commands to history
+setopt APPEND_HISTORY         # Allow multiple terminal sessions to all append to one zsh command history
+setopt INC_APPEND_HISTORY     # Add comamnds as they are typed, don't wait until shell exit
+setopt HIST_EXPIRE_DUPS_FIRST # when trimming history, lose oldest duplicates first
+setopt HIST_IGNORE_ALL_DUPS   # clean up history as commands are executed
+setopt HIST_IGNORE_DUPS       # Do not write events to history that are duplicates of previous events
+setopt HIST_IGNORE_SPACE     # remove command line from history list when first character on the line is a space
+setopt HIST_FIND_NO_DUPS     # When searching history don't display results already cycled through twice
+setopt HIST_SAVE_NO_DUPS     #
+setopt HIST_REDUCE_BLANKS    # Remove extra blanks from each command line being added to history
+setopt HIST_VERIFY           # don't execute, just expand history
+setopt SHARE_HISTORY         # imports new commands and appends typed commands to history
+setopt HIST_IGNORE_SPACE
+setopt HIST_NO_STORE
+setopt HIST_NO_FUNCTIONS
 
 ## Command history configuration
 HISTFILE=$HOME/.zsh_history
