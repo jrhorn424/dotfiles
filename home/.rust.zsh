9a4433642291df0e92ab2961408dfffaff7bd089
Jeffrey Horn <jeff@jrhorn.me>
2018-09-23 19:03:20 -0700

    Add rust configuration

---
+++ home/.rust.zsh
@@ -0,0 +1,9 @@
+# It will add the cargo, rustc, rustup and other commands to Cargo's bin
+# directory, located at:
+#
+#   /Users/jeffh/.cargo/bin
+#
+# You can uninstall at any time with rustup self uninstall and these changes will
+# be reverted.
+
+source $HOME/.cargo/env
